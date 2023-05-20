73fcae0ebe8603344c86ac51858f3e84e49deafe
Kenny Egun <kennyegun240@gmail.com>
2023-05-20 15:39:43 +0100

    Post model relationships and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_many :post_comments
+    has_many :post_likes
+    validates :text, presence: true
+
+    after_save :UpdatePostCount
+    before_save :set_creator_image
+    after_destroy :ReducePostCount
+
+    def UpdatePostCount
+        author.update(posts_count: author.posts.count)
+    end
+
+    def set_creator_image
+        self.creator_image = author.image
+        self.creator_name = author.name
+    end
+
+    def ReducePostCount
+        author.update(posts_count: author.posts.count)
+    end
 end
