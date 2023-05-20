6d1593de5c79df5f60f838bd5dd054d82e372f44
Kenny Egun <kennyegun240@gmail.com>
2023-05-20 15:41:18 +0100

    User model relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    has_many :posts, foreign_key: 'author_id', class_name: 'Post'
+    has_many :post_comments
+    has_many :post_likes
+    has_many :work_experiences
+
+    validates :name, presence: true
+    validates :user_name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :profession, length: {maximum: 20}
+    validates :header, length: {maximum: 100}
+    validates :about, length: {maximum: 1000}
+    validates :bio, length: {maximum: 500}
 end
