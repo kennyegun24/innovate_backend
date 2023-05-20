9c3ad7c4be5103b49c8c74690631bf7c2bb84db1
Kenny Egun <kennyegun240@gmail.com>
2023-05-20 15:41:46 +0100

    Relationships established

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,2 +1,8 @@
 class WorkExperience < ApplicationRecord
+    belongs_to :user
+    validates :company_name, presence: true
+    validates :title, presence: true
+    validates :description, length: {maximum: 500}
+    validates :start_date, presence: true
+    validates :end_date, presence: true
 end
