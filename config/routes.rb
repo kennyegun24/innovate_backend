cbf3c8f97bebb83c5794cc799616b450de38b489
Kenny Egun <kennyegun240@gmail.com>
2023-05-20 23:04:50 +0100

    Modify

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :posts, only: [:create, :index, :show]
+      resources :post_comments, only: [:create, :destroy]
       get "user/profile", to: 'users#get_profile'
       post "user/login", to: 'users#login'
     end
