3ef0b5fcf4cabcac0fa5c99abd424f789ad76886
Toma John Musa <tomajohn316@yahoo.com>
2023-05-30 21:50:44 +0100

    Establish association bwtween followers and users

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,4 @@
 class Follower < ApplicationRecord
-  belongs_to :user
-  belongs_to :follower_user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :follower_user, class_name: 'User'
 end
