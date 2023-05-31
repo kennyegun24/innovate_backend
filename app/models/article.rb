c2aed5a267b3e7e3ef836fdadfae1e476ccb3e61
Toma John Musa <tomajohn316@yahoo.com>
2023-05-31 17:16:20 +0100

    Add model associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :blogs
+
+  has_many :article_comments
+  has_many :article_likes
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+  has_many :articles
+
+  belongs_to :user, class_name: 'User', foreign_key: :author_id
 end
