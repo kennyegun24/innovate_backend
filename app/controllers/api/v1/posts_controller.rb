6d3bd0e25e5b4197762969b33eb332eb423d7b2f
Kenny Egun <kennyegun240@gmail.com>
2023-06-04 15:27:22 +0100

    Methods to delete posts and get a particular user posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,68 +1,69 @@
 class Api::V1::PostsController < ApplicationController
   skip_before_action :authenticate_request, only: %i[index show]
   def create
-        @post = current_user.posts.create(post_params)
+    @post = current_user.posts.create(post_params)
 
-        if @post.save
-            render json: {status: 'Successful', message: 'Post created'}, status: 201
-        else
-            render json: {status: 'Failed', message: @post.errors.full_messages}, status: 422
-        end
+    if @post.save
+      render json: {status: 'Successful', message: 'Post created'}, status: 201
+    else
+      render json: {status: 'Failed', message: @post.errors.full_messages}, status: 422
+    end
   end
 
   def index
-      @posts = Post.all.order(created_at: :DESC)
-      render json: {status: 'Successful', message: 'All Posts', data: @posts}, status: 200
+    @posts = Post.all.order(created_at: :DESC)
+    render json: {status: 'Successful', message: 'All Posts', data: @posts}, status: 200
   end
   
   def show
-      @post = Post.find(params[:id])
-      render json: {status: 'Successful', message: 'Post', data: @post}, status: 200
+    @post = Post.find(params[:id])
+    render json: {status: 'Successful', message: 'Post', data: @post}, status: 200
   end
 
   def authenticated_index
-      @posts = Post.all.order(created_at: :DESC)
+    @posts = Post.all.order(created_at: :DESC)
 
-      @liked = current_user.post_likes.pluck(:post_id)
+    @liked = current_user.post_likes.pluck(:post_id)
 
-      @all_posts = @posts.map do |post|
-          {
-              **post.attributes.symbolize_keys,
-            liked: @liked.include?(post.id)
-          }
-        end
+    @all_posts = @posts.map do |post|
+      {
+        **post.attributes.symbolize_keys,
+        liked: @liked.include?(post.id)
+      }
+    end
 
-      render json: {status: 'Successful', message: 'All Posts', data: @all_posts}, status: 200
+    render json: {status: 'Successful', message: 'All Posts', data: @all_posts}, status: 200
 
   end
 
   def authenticated_show
-      @post = Post.find(params[:id])
-      @isLiked = @post.post_likes.pluck(:user_id).include?(current_user.id)
+    @post = Post.find(params[:id])
+    @isLiked = @post.post_likes.pluck(:user_id).include?(current_user.id)
 
-      one_post = {
-          **@post.attributes.symbolize_keys,
-          liked: @isLiked
-      }
-      # @post.liked = @isLiked
-      render json: {status: 'Successful', message: 'Post', data: one_post}, status: 200
+    one_post = {
+      **@post.attributes.symbolize_keys,
+      liked: @isLiked
+    }
+    render json: {status: 'Successful', message: 'Post', data: one_post}, status: 200
   end
 
   def current_user_posts
     @posts = current_user.posts.includes(:post_likes).order(created_at: :desc)
-    # @liked = current_user.post_likes.select(:post_id).pluck(:post_id)
-
-    # @all_posts = @posts.map do |post|
-    #   {
-    #     **post.attributes.symbolize_keys,
-    #     liked: @liked.include?(post.id)
-    #   }
-    # end
-
     render json: {message: 'Curren user posts', status: 'Successful', data: @posts}, status: 200
   end
 
+  def other_users_posts
+    @user = User.find(params[:id])
+    @posts = @user.posts.all.order(created_at: :DESC)
+    render json: {message: 'User posts', status: 'Successful', data: @posts}, status: 200
+  end
+
+  def destroy
+    @post = current_user.posts.destroy(params[:id])
+    render json: {message: 'Post Deleted', status: 'Successful'}, status: 200
+  end
+
   def post_params
-      params.require(:post).permit(:text, :image)
+    params.require(:post).permit(:text, :image)
   end
 end
