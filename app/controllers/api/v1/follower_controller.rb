502bb09dff4bc6f83e6c721d867c6f4167a5cb89
Toma John Musa <tomajohn316@yahoo.com>
2023-06-10 14:30:40 +0100

    Add follower controller with create action

---
+++ app/controllers/api/v1/follower_controller.rb
@@ -0,0 +1,17 @@
+class FollowerController < ApplicationController
+  def create
+    @follower = Follower.create(user_id: follower_params, follower_user: current_user)
+
+    if @follower.save
+      render json: { status: 'Success', message: 'You have successfully followed' }, status: 201
+    else
+      render json: { status: 'Error', message: @follower.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def follower_params
+    params.require(:follower).permit(:user_id)
+  end
+end
