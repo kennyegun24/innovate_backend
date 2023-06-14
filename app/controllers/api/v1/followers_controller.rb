c9ff62478482c8428a182c8a62057a34c5d13630
Toma John Musa <tomajohn316@yahoo.com>
2023-06-14 21:15:26 +0100

    Expand endpoint to be able to unfollow

--- app/controllers/api/v1/followers_controller.rb
+++ app/controllers/api/v1/followers_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::FollowersController < ApplicationController
   def create
-    @follower = Follower.create(user_id: params[:user_id], follower_user: current_user)
+    unless current_user.user_following.exists?(params[:user_id])
+      @follower = Follower.create(user_id: params[:user_id], follower_user: current_user)
 
-    if @follower.save
-      render json: { status: 'Success', message: 'You have successfully followed' }, status: 201
+      if @follower.save
+        render json: { status: 'Success', message: 'You have successfully followed' }, status: 201
+      else
+        render json: { status: 'Error', message: @follower.errors.full_messages }, status: 422
+      end
+
     else
-      render json: { status: 'Error', message: @follower.errors.full_messages }, status: 422
+      @follow = Follower.where(user_id: params[:user_id], follower_user_id: current_user.id)
+
+      @follow.destroy
+      render json: { message: 'Unfollowed' }
     end
   end
 
