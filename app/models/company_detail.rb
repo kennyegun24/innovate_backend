f5c76230baea3bb2210f8852abeca8942a54c089
Kenny Egun <kennyegun240@gmail.com>
2023-07-29 04:44:31 +0100

    Add relationships and validations

--- app/models/company_detail.rb
+++ app/models/company_detail.rb
@@ -1,2 +1,13 @@
 class CompanyDetail < ApplicationRecord
+  belongs_to :company
+
+  validates :company_image, presence: true
+  validates :company_name, presence: true
+  validates :capacity, presence: true
+  validates :about, presence: true
+  validates :post_count, presence: true,numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :followers_count, presence: true,numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :open_jobs, presence: true
+  validates :header, presence: true, length: {maximum: 20}
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,14 @@
 class Job < ApplicationRecord
+  belongs_to :company
+  has_many :applied_jobs,dependent: :destroy
+
+  validates :company_image, presence: true
+  validates :company_name, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :expiry_date, presence: true
+  validates :location, presence: true
+  validates :experience_level, presence: true
+  validates :no_of_applicants, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
 end
