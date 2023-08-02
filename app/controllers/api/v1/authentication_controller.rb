4307fd8958b614a164a6c7bdb904ddb4c0047acf
Kenny Egun <kennyegun240@gmail.com>
2023-08-02 09:26:26 +0100

    Modify

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AuthenticationController < ApplicationController
 
     if @user.save
       token = encode_token(user_id: @user.id)
-      render json: {status: 'success', message: 'User created', data: {token: token }}, status: 201
+      render json: {status: 'success', message: 'User created', data: {token: token, use_id: @user.id, type: 'individual' }}, status: 201
     else
       render json: { status: 'Error', message: @user.errors.full_messages }, status: 422
     end
@@ -17,7 +17,7 @@ class Api::V1::AuthenticationController < ApplicationController
 
     if @user&.authenticate(params[:password])
       token = encode_token(user_id: @user.id)
-      render json: {status: 'Success', message: 'Logged in successfully', data: {token: token, user_id: @user.id} },status: 200
+      render json: {status: 'Success', message: 'Logged in successfully', data: {token: token, user_id: @user.id, type: 'individual' } },status: 200
     else
       render json: { status: 'Error', message: 'Invalid Email or Password' },status: 401
     end
