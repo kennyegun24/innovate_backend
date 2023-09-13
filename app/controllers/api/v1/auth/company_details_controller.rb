be95c42b872bd9f3a2274144fbd43425bbfaaa24
Kenny Egun <kennyegun240@gmail.com>
2023-09-13 20:38:03 +0100

    company details controller to show company details and company model

--- app/controllers/api/v1/auth/company_details_controller.rb
+++ app/controllers/api/v1/auth/company_details_controller.rb
@@ -2,4 +2,25 @@ class Api::V1::Auth::CompanyDetailsController < ApplicationController
   # update method
   # show method to display current company details
   # create method to display other company details
+
+  def show
+    @company = Company.find_by(id: 1).as_json(include: [:company_detail])
+    @company_details = {
+      company_name: @company["company_detail"]["company_name"],
+      capacity: @company["company_detail"]["capacity"],
+      creation_date: @company["company_detail"]["creation_date"],
+      about: @company["company_detail"]["about"],
+      post_count: @company["company_detail"]["post_count"],
+      followers_count: @company["company_detail"]["followers_count"],
+      open_jobs: @company["company_detail"]["open_jobs"],
+      company_image: @company["company_detail"]["company_image"],
+      company_id: @company["company_detail"]["company_id"],
+      header: @company["company_detail"]["header"],
+      email: @company["email"]
+    }
+    render json: {
+            status: 200, data:
+            @company_details
+          }, status: 200
+  end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,10 @@ class Company < ApplicationRecord
   has_secure_password
   has_one :company_detail
   has_many :jobs
+  has_many :followers, foreign_key: :company_id, class_name: 'Follower'
+  has_many :user_followers, through: :followers, source: :follower_user
 
+  has_many :followings, foreign_key: :company_id, class_name: 'Following'
+  has_many :user_following, through: :followings, source: :following_user
   validates :email, presence: true, uniqueness: true
 end
